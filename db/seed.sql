5877b1c44d77815c919191a5cadc42debcb28a4a
cs-witch <andisa.andisa@gmail.com>
2020-03-02 14:54:13 -0500

    first commit

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,17 @@
 INSERT INTO creature_types(type, description, img_url)
-VALUES 
+VALUES
     ('Nyan Cat', 'A mystical flying space feline in the shape of a pop tart.', 'https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG'),
     ('Dragon', 'A reptile in varired sizes and magical abilities.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Dragon.svg/1200px-Dragon.svg.png'),
     ('Unicorn', 'A equine animal, typically with white fur, easily identifed by a large horn on their heads', 'https://upload.wikimedia.org/wikipedia/en/7/78/The_Hunt_of_the_Unicorn_Tapestry_7.jpg');
+
+INSERT INTO adoptable_creatures(
+    name,
+    img_url,
+    age,
+    vaccination_status,
+    adoption_story,
+    adoption_status,
+    type_id
+)
+VALUES
+    ('Nyan Nyan',)
