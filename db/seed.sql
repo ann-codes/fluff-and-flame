ea3d0d9b2d67540755afb8eb4fb253dc6ecbbfc1
cs-witch <andisa.andisa@gmail.com>
2020-03-02 15:24:44 -0500

    populated data

--- db/seed.sql
+++ db/seed.sql
@@ -15,4 +15,4 @@ INSERT INTO adoptable_creatures(
 VALUES
     ('Nyan Nyan', 'https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG', 4, 'no', 'Was found at the end of a rainbow and is looking for a new sky home', 'adoption pending'),
     ('Spyro', 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2018/11/spyro_reignited.png', 7950, '', 'Fought many a battle now needs a happy home', 'can be adopted'),
-    ('Peter Pegasus', 'https://vignette.wikia.nocookie.net/disney/images/e/e7/Peter_Pegasus.jpg/revision/latest?cb=20180601060225', 2, 'no', 'Fell from the clouds and into you arms', 'can be adopted');
+    ('Peter Pegasus', 'https://vignette.wikia.nocookie.net/disney/images/e/e7/Peter_Pegasus.jpg/revision/latest?cb=20180601060225', 2, 'no', 'Fell from the clouds and into your arms', 'can be adopted');
