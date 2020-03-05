cfcb0fc53a055033b3c8e58d741fea8667d3fccc
Ann <ann.nguyen.codes@gmail.com>
2020-03-05 14:13:42 -0500

    save to merge conflicts

--- src/components/CreatureTypeShowpage.js
+++ src/components/CreatureTypeShowpage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { Redirect } from "react-router-dom";
 import CreatureInformation from "./CreatureInformation";
 import NotFound404 from "./NotFound404";
 import fetchData from "../functions/fetchData";
