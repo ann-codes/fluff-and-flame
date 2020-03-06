32c839f2f1101d3ad6a410faf718538d1bc2e794
Ann <ann.nguyen.codes@gmail.com>
2020-03-06 10:54:20 -0500

    tweaks

--- src/components/NotFound404.js
+++ src/components/NotFound404.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const NotFound404 = () => <h1 className="fade-in">404 NOT FOUND!</h1>;
+const NotFound404 = () => (
+  <div className="text-center">
+    <h1 className="fade-in">404 NOT FOUND!</h1>
+    <img src="https://i.pinimg.com/originals/e2/5f/f0/e25ff08c3cd25a1a64187aa83bf7c6c0.jpg" className="img-tweaks fade-in" />
+  </div>
+);
 
 export default NotFound404;
